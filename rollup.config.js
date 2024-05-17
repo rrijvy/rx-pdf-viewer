40ef90f2f94eac6a5c69dbd6494763f211fe229a
rrijvy <rrijvy@gmail.com>
2024-05-17 20:11:12 +0600

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import json from "@rollup/plugin-json";
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -20,7 +21,7 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+    plugins: [resolve(), commonjs(), json(), typescript({ tsconfig: "./tsconfig.json" })],
   },
   {
     input: "dist/esm/types/index.d.ts",
--- src/components/pdfViewer.tsx
+++ src/components/pdfViewer.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+// import * as pdfjsLib from "pdfjs-dist";
+import { useEffect } from "react";
 
 const PdfViewer = () => {
-  return <div></div>;
+  useEffect(() => {
+    (function () {
+      // console.log(pdfjsLib);
+    })();
+  }, []);
+  return <div className="rx-pdf-viewer">Pdf Viewer</div>;
 };
 
 export default PdfViewer;
