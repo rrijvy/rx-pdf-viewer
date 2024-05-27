75d269614e6c162e59e216bc15139726c0ee39cf
rrijvy <rrijvy@gmail.com>
2024-05-27 18:24:45 +0600

    wip

--- src/pdfViewer.tsx
+++ src/pdfViewer.tsx
@@ -13,20 +13,76 @@ const PdfViewer: FC<PdfViewerProps> = (props: PdfViewerProps) => {
   const pdfRef = useRef<PDFDocumentProxy>();
   const containerRef = useRef<HTMLElement>();
   const pageRendering = useRef(false);
+  const pdfjs = useRef(false);
 
   useEffect(() => {
     (async (param?: PdfViewerProps) => {
       pdfjsLib.GlobalWorkerOptions.workerSrc = props.workerSrc;
       const pdfUrl = "https://p1stonimage.s3.amazonaws.com/ManualImportTemplate/dev/Baula+Ke+Banailo+Re+By+Humayun+Ahmed.pdf";
       const pdf = await pdfjsLib.getDocument(pdfUrl).promise;
-      console.log("::: Pdf Viewer", pdfRef.current);
+      pdfRef.current = pdf;
+      renderPage(1, 1);
     })(props);
   }, []);
 
-  console.log("::: Pdf viewer.");
-  console.log("::: pdfjsLib - ", pdfjsLib);
+  const renderPage = (pageNum: number, zoomLevel?: number) => {
+    pageRendering.current = true;
 
-  return <div>Pdf Viewer</div>;
+    if (pdfRef.current && canvasRef.current) {
+      let scale = zoomLevel || 1,
+        canvas = canvasRef.current,
+        ctx = canvasRef.current?.getContext("2d");
+
+      pdfRef.current.getPage(pageNum).then((page) => {
+        if (!ctx || !textLayeyRef.current) return;
+
+        textLayeyRef.current.setAttribute("class", "textLayer");
+        textLayeyRef.current.style.setProperty("--scale-factor", scale.toString());
+        textLayeyRef.current.style.left = canvasRef.current?.offsetLeft + "px";
+        textLayeyRef.current.style.top = canvasRef.current?.offsetTop + "px";
+
+        let viewport = page.getViewport({ scale: scale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        if (containerRef.current && containerRef.current.clientWidth < viewport.width) {
+          if (pdfContainerRef.current) pdfContainerRef.current.style.left = "0";
+        } else {
+          if (pdfContainerRef.current) pdfContainerRef.current.style.left = "auto";
+        }
+
+        let renderContext = {
+          canvasContext: ctx,
+          viewport: viewport,
+        };
+
+        const renderTask = page.render(renderContext);
+        renderTask.promise
+          .then(() => {
+            pageRendering.current = false;
+            return page.getTextContent();
+          })
+          .then((textContent) => {
+            if (textLayeyRef.current)
+              pdfjsLib.renderTextLayer({
+                textContentSource: textContent,
+                container: textLayeyRef.current,
+                viewport: viewport,
+                textDivs: [],
+              });
+          });
+      });
+    }
+  };
+
+  console.log("::: Pdf viewer loaded :::");
+
+  return (
+    <div ref={pdfContainerRef}>
+      <canvas ref={canvasRef} />
+      <div ref={textLayeyRef} />
+    </div>
+  );
 };
 
 export default PdfViewer;
